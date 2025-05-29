dad82e70049a32f2bb7c0964fea1db2f6df7c09d
Pedro Henrique SS <butijao.cromado5@gmail.com>
2025-05-29 02:35:13 -0300

    vamo laaa

---
+++ src/components/Exemplo2.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export function Exemplo2() {
+  const [cep, setCep] = useState("")
+  const [logradouro, setLogradouro] = useState("")
+  const [bairro, setBairro] = useState("")
+  const [cidade, setCidade] = useState("")
+  const [estado, setEstado] = useState("")
+  const [uf, setUf] = useState("")
+
+  useEffect(() => {
+    if (cep.length === 8) {
+      axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+        setLogradouro(response.data.logradouro)
+        setBairro(response.data.bairro)
+        setCidade(response.data.cidade)
+        setEstado(response.data.estado)
+        setUf(response.data.uf)
+      })
+    }
+  }, [cep])
+
+  return (
+    <section>
+      <h2>Exemplo 2: Buscar endereço</h2>
+
+      <div>
+        <input
+          type="number"
+          placeholder="Digite seu CEP"
+          onChange={(input) => setCep(input.target.value)}
+        />
+
+        {cep.length === 8 && (
+          <>
+            <h3>Informações sobre o CEP:</h3>
+            <ul>
+              <li>Logradouro: {logradouro}</li>
+              <li>Bairro: {bairro}</li>
+              <li>Cidade: {cidade}</li>
+              <li>
+                Estado: {estado} - {uf}
+              </li>
+            </ul>
+          </>
+        )}
+      </div>
+    </section>
+  )
+}
